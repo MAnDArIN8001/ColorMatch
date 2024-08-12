62adedcbe53cdbcbd7a435107317d1a522c3adca
Artem <artem15stepanenko@outlook.com>
2024-08-12 11:29:59 +0300

    End with color controller

--- Assets/Scripts/Cart/CartColorController.cs
+++ Assets/Scripts/Cart/CartColorController.cs
@@ -1,9 +1,14 @@
+using System;
 using UnityEngine;
 
 public class CartColorController : MonoBehaviour
 {
+    public event Action<Color> OnColorChanged;
+
     [SerializeField] private float _minChangeTime;
     [SerializeField] private float _maxChangeTime;
+    private float _randomChangeTime;
+    private float _currentChangeTime;
 
     [SerializeField] private Color[] _colors;
 
@@ -11,8 +16,37 @@ public class CartColorController : MonoBehaviour
 
     [SerializeField] private MeshRenderer _modelRenderer;
 
-    private void Awake()
+    private void Start()
     {
-        
+        ChangeColor();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_currentChangeTime <= 0)
+        {
+            ChangeColor();
+
+            _randomChangeTime = UnityEngine.Random.Range(_minChangeTime, _maxChangeTime);
+            _currentChangeTime = _randomChangeTime;
+        }
+
+        _currentChangeTime -= Time.fixedDeltaTime;
+    }
+
+    private void ChangeColor()
+    {
+        Color newColor = GetRandomColor();
+
+        OnColorChanged?.Invoke(newColor);
+
+        _modelRenderer.material.color = newColor;
+    }
+
+    private Color GetRandomColor()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, _colors.Length);
+
+        return _colors[randomIndex];
     }
 }
